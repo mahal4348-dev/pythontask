2ee9073d33546e1a518c65f4894b8074f85106d7
Maha Lakshmi <mahal4348@gmail.com>
2026-09-21 12:27:46 +0530

    index.py

---
+++ index.py
@@ -0,0 +1,43 @@
+# index single
+name = ("maha")
+print(name[0])
+print(name[0:3])
+
+# index multi string
+name = ("anu","maha","lubu")
+print(name[0])
+print(name[0:2])
+
+#index single number
+#age=21 Single value(int) → indexing panna mudiyathu
+#print(age[0])
+#Single-value tuple
+age=(21,)
+print(age[0])
+
+#Multiple-value tuple
+age=(21,22)
+print(age[1])
+
+
+#Sequence single string
+name = ("anu")
+for i in name:
+    print(i)
+
+# Sequence multi string
+name = ("maha","lubu","anu")
+for i in name:
+    print(i)
+
+#sequence single int
+age = ("21")
+for i in age:
+    print(i)
+
+age = (21,23)
+for i in age:
+    print(i)
+
+
+
